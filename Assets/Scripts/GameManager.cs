6539c7d526c364847f0c9f4a5c273de6b5539da0
Michael <zhdelong@foxmail.com>
2016-04-09 14:38:52 +0800

    M

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager _instance;
+    public int score = 0;
+    public Text scoreShow;
+
+    void Awake() {
+        _instance = this;        
+    }
+	// Use this for initialization
+	void Start () {
+        scoreShow.text = "Score :123";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero : MonoBehaviour {
+
+    public bool heroAnimation = true;
+    public int frameCountPerSecond = 10;
+    public float timer = 0;
+    public Sprite[] sprites;
+    private SpriteRenderer spriteRender;
+    public float superGunTime = 10f;
+    private float resetSupterGunTime;
+
+    private bool isMouseDown = false;
+    private Vector3 lastMousePosition = Vector3.zero;
+    private int gunCount = 1;
+
+    public Gun gunTop;
+    public Gun gunLeft;
+    public Gun gunRight;
+	// Use this for initialization
+	void Start () {
+        spriteRender = this.GetComponent<SpriteRenderer>();
+        resetSupterGunTime = superGunTime;
+        superGunTime = 0;
+        gunTop.openFire();
+	}
+  
+	// Update is called once per frame
+	void Update () {
+        if (heroAnimation) {
+            timer += Time.deltaTime;
+            int frameIndex = (int)(timer / (1f / frameCountPerSecond));//the same to static frame++ in Update
+            int frame = frameIndex % 2;
+            spriteRender.sprite = sprites[frame];
+        }
+
+        if (Input.GetMouseButtonDown(0)) {
+            isMouseDown = true;
+        }
+        if (Input.GetMouseButtonUp(0)) {
+            isMouseDown = false;
+            lastMousePosition = Vector3.zero;
+        }
+
+        if (isMouseDown) {
+            if (lastMousePosition != Vector3.zero) {
+                //Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector3 offset = Camera.main.ScreenToWorldPoint(Input.mousePosition) - lastMousePosition;
+                
+                transform.position = transform.position + offset;
+                checkPosition();
+                
+            }
+
+            lastMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (superGunTime > 0) {
+            superGunTime-=Time.deltaTime;
+            if (gunCount == 1) {
+                transformToSuperGun();
+            }
+        } else if (gunCount == 2) {
+            transformToNormalGun();
+        }
+	}
+
+    private void transformToSuperGun() {
+        gunCount = 2;
+        gunLeft.openFire();
+        gunRight.openFire();
+        gunTop.stopFire();
+    }
+
+    private void transformToNormalGun() {
+        gunCount = 1;
+        gunLeft.stopFire();
+        gunRight.stopFire();
+        gunTop.openFire();
+    }
+    private void checkPosition() {
+        //check x -2.22f~2.22f  y -3.9f~3.4f
+        Vector3 pos = transform.position;
+        float x = pos.x;
+        float y = pos.y;
+        x = x < -2.22f?-2.22f : x;
+        x = x > 2.22f ? 2.22f : x;
+        y = y < -3.9f ? -3.9f : y;
+        y = y > 3.4f ? 3.4f : y;
+
+        transform.position = new Vector3(x, y, 0);
+    }
+
+    public void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Award") {
+            Award award = other.GetComponent<Award>();
+            if (award.type == 0) {
+                //chang to double guns
+                superGunTime = resetSupterGunTime;
+                Destroy(award.gameObject);
+            }
+        }
+    }
+    
+}
